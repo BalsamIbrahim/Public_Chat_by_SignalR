7f0b7786c0a2ddf88252860ffef49af744a6a441
Balsam Alaghawane <b.alaghawane@hotmail.com>
2020-09-19 17:54:48 +0300

    Add project files.

---
+++ SignalR/wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+connection.on("ReciveMessage", function (fromuser, message) {
+    var msg = fromuser + ": " + message;
+    var li = document.createElement("li");
+    li.textContent = msg;
+    $('#list').append(li);
+});
+
+connection.start();
+$('#btnsend').click(function () {
+    var fromuser = $('#txtuser').val();
+    var fromtext = $('#txtmessage').val();
+
+    connection.invoke("SendMessage", fromuser, fromtext );
+});
